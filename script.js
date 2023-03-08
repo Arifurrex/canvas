b3c3a5895e9c714e8ce60aa290f52e8c30eea959
arifur rahman <arifurrex@gmail.com>
2023-03-09 02:22:14 +0600

    mousehoverparticleeffect

--- script.js
+++ script.js
@@ -1,68 +1,95 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const particleSpot = [];
+console.log(canvas);
+console.log(ctx);
+canvas.width = window.innerHeight;
+canvas.height = window.innerHeight;
+let spots = [];
 let hue = 0;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-window.addEventListener('resize', function () {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-})
-
-
 const mouse = {
-    x: undefined,
-    y: undefined
+    x:undefined,
+    y:undefined
 }
+
 canvas.addEventListener('mousemove', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
-    for (let i = 0; i < 3; i++){
-        particleSpot.push(new Particle());
+    for (let i = 0; i < 4; i++) {
+        spots.push(new Particle());
     }
-    console.log(particleSpot.length);
-    
 })
-
 class Particle{
     constructor() {
-        console.log(mouse.x);
-        console.log(mouse.y);
-        this.x = mouse.x
+        this.x = mouse.x;
         this.y = mouse.y;
-        this.size = Math.random() * 4 + 1.5;
-        this.speedX = Math.random() * 3 - 1.5;
-        this.speedY = Math.random() * 3 - 1.5;
-        this.color = `hsl(${hue * Math.random()*100 + 1},${Math.random()*80 + 1}%,40%)`;
-        // this.color = `hsl(140,70%,80%)`;
-        console.log(particleSpot);
+        this.size = Math.random() * 2 + 0.1;
+        this.speedX = Math.random() * 2 - 1;
+        this.speedY = Math.random() * 2 - 1 ;
+        this.color = `hsl(${hue},50%,80%)`;
     }
-    
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
+        if (this.size > 0.1) {
+            this.size -= 0.03;
+        }
     }
     draw() {
-        ctx.strokeStyle = this.color;
+        ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.stroke();
+        ctx.fill();
     }
 }
 
 function handleParticle() {
-    // console.log(particleSpot.length);
-    for (let i = 0; i < particleSpot.length; i++){
-        particleSpot[i].update();
-        particleSpot[i].draw();
+    for (let i = 0; i < spots.length; i++){
+        spots[i].update();
+        spots[i].draw();
+        for (j = 0; j < spots.length; j++){
+            const dx = spots[i].x - spots[j].x;
+            const dy = spots[i].y - spots[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < 90) {
+                ctx.beginPath();
+                ctx.strokeStyle = spots[i].color;
+                ctx.lineWidth = spots[i].size / 10;
+                ctx.moveTo(spots[i].x, spots[i].y);
+                ctx.lineTo(spots[j].x, spots[j].y);
+                ctx.stroke();
+
+            }
+        }
+        if (spots[i].size <= 0.3) {
+            spots.splice(i, 1);
+            i--;
+        }
     }
 }
-function animation() {
+function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     handleParticle();
     hue++;
-    requestAnimationFrame(animation);
+    requestAnimationFrame(animate);
 }
-animation();
+window.addEventListener('resize', function () {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    init();
+})
+window.addEventListener('mouseout', function () {
+    mouse.x = undefined;
+    mouse.y = undefined;
+})
+animate();
+
+console.log(Math.random() * 2 + 0.1);
+console.log(Math.random() * 2 - 1);
+console.log(Math.random() * 2 - 1);
+console.log(`hsl(${hue},100%,50%)`);
+
+
+
+
+
