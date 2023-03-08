b7edf53aa30548708b0ffae3b35d8a4caaea7b3c
arifur rahman <arifurrex@gmail.com>
2023-03-09 02:30:36 +0600

    roundmousepointer

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d');
 console.log(ctx);
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// how to make paint brush 
-const mouse = {
-    x: undefined,
-    y: undefined
-}
 window.addEventListener('resize', function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 })
+const mouse = {
+    x: undefined,
+    y: undefined
+}
 
 canvas.addEventListener('click', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
+    console.log(event.x);
     draw();
 })
-
 canvas.addEventListener('mousemove', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
@@ -28,8 +26,15 @@ canvas.addEventListener('mousemove', function (event) {
 })
 
 function draw() {
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = "red";
     ctx.beginPath();
-    ctx.arc(mouse.x, mouse.y, 10, 0, Math.PI * 2);
+    ctx.arc(mouse.x, mouse.y, 20, 0, Math.PI * 2);
     ctx.fill();
 }
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    draw();
+    requestAnimationFrame(animate);
+}
+animate();
